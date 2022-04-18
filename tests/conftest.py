742127a0729d566bffa9d40cf2111086ac5d7492
Kate <katia.yurevna@gmail.com>
2022-04-18 10:53:16 -0700

    fix doc strings in connftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,15 @@
+""" The module used for setting up pytest command line arguments"""
+
 from pytest import fixture
 
-
 def pytest_addoption(parser):
+    """pass a parser object"""
     parser.addoption("--test_config", action="store")
 
 
 @fixture(scope="session")
 def name(request):
+    """Fixture function"""
     name_value = request.config.option.test_config
     print(name_value)
     return True
